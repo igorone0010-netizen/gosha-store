006cdbf04f0fbfb1f0200219179570d01f7b2df5
igorone0010-netizen <igor.one0010@gmail.com>
2025-10-04 04:25:27 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -443,6 +443,9 @@ document.addEventListener('DOMContentLoaded', function() {
     showMain();
     updateProductsCount();
     
+    // ДОБАВЬТЕ ЭТУ СТРОКУ ↓
+    initCarousel();
+    
     // Скрываем навигацию при загрузке (на главной)
     document.getElementById('nav-panel').classList.remove('active');
     
